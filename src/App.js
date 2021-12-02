6b010f40b9d12fdb727109d3fd510aa48e203ff3
Pedro Machado <machadop1407@gmail.com>
2021-12-01 19:08:33 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/products" />
+        <Route path="/contact" />
+        <Route path="/about" />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import {
+  NavbarContainer,
+  LeftContainer,
+  RightContainer,
+  NavbarExtendedContainer,
+  NavbarInnerContainer,
+  NavbarLinkContainer,
+  NavbarLink,
+  Logo,
+  OpenLinksButton,
+  NavbarLinkExtended,
+} from "../styles/Navbar.style";
+import LogoImg from "../assets/logo.png";
+
+function Navbar() {
+  const [extendNavbar, setExtendNavbar] = useState(false);
+
+  return (
+    <NavbarContainer extendNavbar={extendNavbar}>
+      <NavbarInnerContainer>
+        <LeftContainer>
+          <NavbarLinkContainer>
+            <NavbarLink to="/"> Home</NavbarLink>
+            <NavbarLink to="/products"> Products</NavbarLink>
+            <NavbarLink to="/contact"> Contact Us</NavbarLink>
+            <NavbarLink to="/about"> About Us</NavbarLink>
+            <OpenLinksButton
+              onClick={() => {
+                setExtendNavbar((curr) => !curr);
+              }}
+            >
+              {extendNavbar ? <>&#10005;</> : <> &#8801;</>}
+            </OpenLinksButton>
+          </NavbarLinkContainer>
+        </LeftContainer>
+        <RightContainer>
+          <Logo src={LogoImg}></Logo>
+        </RightContainer>
+      </NavbarInnerContainer>
+      {extendNavbar && (
+        <NavbarExtendedContainer>
+          <NavbarLinkExtended to="/"> Home</NavbarLinkExtended>
+          <NavbarLinkExtended to="/products"> Products</NavbarLinkExtended>
+          <NavbarLinkExtended to="/contact"> Contact Us</NavbarLinkExtended>
+          <NavbarLinkExtended to="/about"> About Us</NavbarLinkExtended>
+        </NavbarExtendedContainer>
+      )}
+    </NavbarContainer>
+  );
+}
+
+export default Navbar;
